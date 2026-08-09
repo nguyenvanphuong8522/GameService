15aa9b90bd7b10b0853b67e1cea06e0bd781faee
nguyenvanphuong <nguyenvanphuong8522@gmail.com>
2026-08-10 06:27:16 +0700

    add tunnel

--- DemoRestApi3/obj/Debug/net8.0/DemoRestApi3.AssemblyInfo.cs
+++ DemoRestApi3/obj/Debug/net8.0/DemoRestApi3.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("DemoRestApi3")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+004d3061cce43867dc35e6d95a3a79bf5c389822")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8aff9fe9d5bb2df4bff3619ae648584b5e2490ca")]
 [assembly: System.Reflection.AssemblyProductAttribute("DemoRestApi3")]
 [assembly: System.Reflection.AssemblyTitleAttribute("DemoRestApi3")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
